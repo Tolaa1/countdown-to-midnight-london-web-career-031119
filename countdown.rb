043eaec0cc4b65d62fd12897a4722600babf4cd8
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-02-20 01:33:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,3 @@
-#write your code here
-
-#def countdown(seconds_to_midnight)
- # while seconds_to_midnight > 0 
-  #puts "#{seconds_to_midnight} SECOND(S)!"
-  #sleep (5)
-  #seconds_to_midnight -= 1 
-#end
- #seconds_to_midnight.to_s + "HAPPY NEW YEAR!"
-#end
 
 def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
